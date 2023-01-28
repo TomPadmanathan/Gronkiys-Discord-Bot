7a24f2caac72c1db82a45712ee51ec6a8003ada0
Tom Padmanathan <padmanathantom@gmail.com>
2023-01-28 00:12:14 +0000

    added timeout command

--- src/main.js
+++ src/main.js
@@ -29,6 +29,7 @@ function sendEmailAlert(info) {
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">Helper Id</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">User Username</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">User Id</td>
+                <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">Duration</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">Reason</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">DateTime</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">Attachment</td>
@@ -39,6 +40,7 @@ function sendEmailAlert(info) {
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">${info.helper.id}</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">${info.user.username}</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">${info.user.id}</td>
+                <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">${info.duration}</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">${info.reason}</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">${info.datetime}</td>
                 <td style="border: 1px solid black; border-collapse: collapse; padding: 10px;">${info.attachment}</td>
@@ -67,7 +69,7 @@ function commitPunishmentToDatabase(info) {
         console.log('Connected to the MySQL server.');
 
         connection.query(
-            `INSERT INTO punishments(PunishmentType, HelperUsername, HelperId, UserUsername, UserId, Reason, Datetime, Attachment) VALUES ('${info.punishmentType}', '${info.helper.username}', '${info.helper.id}', '${info.user.username}', '${info.user.id}', '${info.reason}', '${info.datetime}', '${info.attachment}');`,
+            `INSERT INTO punishments(PunishmentType, HelperUsername, HelperId, UserUsername, UserId, Duration, Reason, Datetime, Attachment) VALUES ('${info.punishmentType}', '${info.helper.username}', '${info.helper.id}', '${info.user.username}', '${info.user.id}', '${info.duration}', '${info.reason}', '${info.datetime}', '${info.attachment}');`,
             (err, result) => {
                 if (err) throw err;
                 console.log('Inserted info in database');
@@ -143,6 +145,7 @@ class CommandFormat1 {
                 client.users.cache.get(this.userid(message)).discriminator,
         };
         this.reason = this.reason(message);
+        this.duration = undefined;
         this.attachment = this.attachment(message);
         this.datetime = new Date(message.createdTimestamp)
             .toJSON()
@@ -181,6 +184,83 @@ class CommandFormat2 {
             username: message.content[2],
         };
         this.reason = this.reason(message);
+        this.duration = undefined;
+        this.attachment = this.attachment(message);
+        this.datetime = new Date(message.createdTimestamp)
+            .toJSON()
+            .slice(0, 19)
+            .replace('T', ' ');
+    }
+}
+
+class CommandFormat3 {
+    // In server
+    // type @user reason
+    userid(message) {
+        let userid = message.content[1].split('');
+        for (let i = 0; i < userid.length; i++) {
+            if (
+                userid[i] == '<' ||
+                userid[i] == '>' ||
+                userid[i] == '@' ||
+                userid[i] == '!'
+            ) {
+                delete userid[i];
+            }
+        }
+        return userid.join('');
+    }
+    reason(message) {
+        let reason = message.content[3];
+        for (let i = 4; i < message.content.length; i++) {
+            reason = reason + ' ' + message.content[i];
+        }
+        return reason;
+    }
+    attachment(message) {
+        try {
+            return message.attachments.first().url;
+        } catch {
+            return undefined;
+        }
+    }
+    duration(message) {
+        const durationMultiplyer = {
+            s: 1_000,
+            m: 60_000,
+            h: 3_600_000,
+            d: 86_400_000,
+        };
+
+        let duration = message.content[2].split('');
+        duration.pop();
+
+        return (
+            Number(duration.join('')) *
+            durationMultiplyer[
+                message.content[2].split('')[
+                    message.content[2].split('').length - 1
+                ]
+            ]
+        );
+    }
+
+    constructor(message) {
+        this.punishmentType = message.content[0];
+        this.helper = {
+            id: message.author.id,
+            username:
+                message.author.username + '#' + message.author.discriminator,
+        };
+        this.user = {
+            id: this.userid(message),
+            username:
+                client.users.cache.get(this.userid(message)).username +
+                '#' +
+                client.users.cache.get(this.userid(message)).discriminator,
+        };
+        this.reason = this.reason(message);
+        this.duration = this.duration(message);
         this.attachment = this.attachment(message);
         this.datetime = new Date(message.createdTimestamp)
             .toJSON()
@@ -313,11 +393,38 @@ client.on('messageCreate', async message => {
 
         sendEmailAlert(info);
         commitPunishmentToDatabase(info);
+    } else if (message.content[0] === 'timeout') {
+        let info;
+        try {
+            info = new CommandFormat3(message);
+        } catch {
+            message.reply('An error has occured obtaining user info');
+            return;
+        }
+
+        if (!checkCommandIsValid(info, client, message)) return;
+
+        try {
+            const member = message.mentions.members.first();
+            member.timeout(info.duration, info.reason);
+            message.reply(
+                `Timed out ${info.user.username} for ${info.reason}.`
+            );
+        } catch {
+            message.reply(
+                `An error has occured ${info.punishmentType + 'ing'} the user`
+            );
+            return;
+        }
+
+        sendEmailAlert(info);
+        commitPunishmentToDatabase(info);
     } else if (message.content[0] === 'help') {
         message.reply(
             `
             > **Ban**:\n > \`${prefix}ban 'usermention' 'reason'\` \n > \`${prefix}ban @BabyMole Spamming in general\` \n
             > **Kick**:\n > \`${prefix}kick 'usermention' 'reason'\` \n > \`${prefix}kick @BabyMole Spamming in general\` \n
+            > **Timeout**:\n > \`${prefix}timeout 'usermention' 'duration' 'reason'\` \n > \`${prefix}timeout @BabyMole 6d Spamming in general\` \n
             > **Unban**:\n > \`${prefix}unban 'userid' 'username' 'reason'\` \n > \`${prefix}unban 556184736251248659 BabyMole#5476 Mistakenly banned\`
             `
         );
